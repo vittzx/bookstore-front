90df062d9a34e38878e07845f29d8cf919088ab2
Vitor Cosso <vitorcosso@gmail.com>
2023-12-19 15:28:30 -0300

    feat: adding footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/template/header/header.component';
 import { ContainerComponent } from "./components/template/container/container.component";
+import { FooterComponent } from "./components/template/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: 'app.component.html',
     styles: [],
-    imports: [CommonModule, RouterOutlet, HeaderComponent, ContainerComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, ContainerComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'bookstore-front';
--- src/app/components/template/container/container.component.ts
+++ src/app/components/template/container/container.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
-  selector: 'app-container',
-  standalone: true,
-  imports: [],
-  templateUrl: './container.component.html',
-  styleUrl: './container.component.css'
+    selector: 'app-container',
+    standalone: true,
+    templateUrl: './container.component.html',
+    styleUrl: './container.component.css',
+    imports: []
 })
 export class ContainerComponent {
 
