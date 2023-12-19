7c399c2818cbf51f8afefd8f6b1aee59937658d2
Vitor Cosso <vitorcosso@gmail.com>
2023-12-18 21:41:44 -0300

    feat: adding header and main style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/template/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: 'app.component.html',
   styles: [],
 })
