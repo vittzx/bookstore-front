5a13f8b9ef42bfdf907dc1e655405be8cfe6f013
Vitor Cosso <vitorcosso@gmail.com>
2023-12-18 22:05:41 -0300

    feat: container pt1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/template/header/header.component';
+import { ContainerComponent } from "./components/template/container/container.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
-  templateUrl: 'app.component.html',
-  styles: [],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: 'app.component.html',
+    styles: [],
+    imports: [CommonModule, RouterOutlet, HeaderComponent, ContainerComponent]
 })
 export class AppComponent {
   title = 'bookstore-front';
