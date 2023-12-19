484b5ad7b8baeedf64eb2377c7ef66fc110c853f
Vitor Cosso <vitorcosso@gmail.com>
2023-12-19 18:44:05 -0300

    feat: category and enviromenst

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ContainerComponent } from './components/template/container/container.component';
+import { CategoriaComponent } from './components/views/categoria/categoria.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ContainerComponent
+    },
+    {
+        path: 'category',
+        component: CategoriaComponent
+    }
+];
