f47106492e6101209d8b5d27517de2ebabe48c82
Vitor Cosso <vitorcosso@gmail.com>
2023-12-20 15:56:08 -0300

    feat: starting category component

--- src/app/components/views/categoria/categoria.component.ts
+++ src/app/components/views/categoria/categoria.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './categoria.component.css'
 })
 export class CategoriaComponent {
+  categoryGeneral = {
+    id: 1,
+    name: "Test",
+    description: "Test"
+  };
 
+
+  categoriesGeneral =[this.categoryGeneral, 
+  {
+    id: 2,
+    name: "Test2",
+    description: "test2"
+  }]
 }
