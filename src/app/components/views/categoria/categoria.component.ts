1ebf674aa2c348add8af432432b6dfa3c7b8f62a
Vitor Cosso <vitorcosso@gmail.com>
2023-12-21 00:07:50 -0300

    feat: adding categories buttons

--- src/app/components/views/categoria/categoria.component.ts
+++ src/app/components/views/categoria/categoria.component.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-categoria',
-  standalone: true,
-  imports: [],
-  templateUrl: './categoria.component.html',
-  styleUrl: './categoria.component.css'
+    selector: 'app-categoria',
+    standalone: true,
+    templateUrl: './categoria.component.html',
+    styleUrl: './categoria.component.css',
+    imports: []
 })
 export class CategoriaComponent {
-  categoryGeneral = {
-    id: 1,
-    name: "Test",
-    description: "Que inconstância é essa, menina? Que coisa é essa que você quer e após dois minutos enjoa?"
-  };
-
-
-  categoriesGeneral =[this.categoryGeneral, 
-  {
-    id: 2,
-    name: "Test2",
-    description: "test2"
-  }]
-}
+    categoryGeneral = {
+        id: 1,
+        name: "Test",
+        description: "Que inconstância é essa, menina? Que coisa é essa que você quer e após dois minutos enjoa?"
+      };
+    
+    
+      categoriesGeneral =[this.categoryGeneral, 
+      {
+        id: 2,
+        name: "Test2",
+        description: "test2"
+      }]
+    
+ }
